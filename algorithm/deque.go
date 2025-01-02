d0641ad634a86ff50548e08504583418cc801991
Today <76273733+today2098@users.noreply.github.com>
2025-01-02 18:35:02 +0900

    [update] Add descriptions about each methods of "Deque"

--- algorithm/deque.go
+++ algorithm/deque.go
@@ -2,11 +2,13 @@
 
 package algorithm
 
+// Deque represents Double-ended queue data structure.
 type Deque[T any] struct {
 	FrontStack []T
 	BackStack  []T
 }
 
+// NewDeque returns an initialized Deque.
 func NewDeque[T any]() *Deque[T] {
 	return &Deque[T]{
 		FrontStack: make([]T, 0),
@@ -14,14 +16,17 @@ func NewDeque[T any]() *Deque[T] {
 	}
 }
 
+// Empty reports whether the deque is empty.
 func (dq *Deque[T]) Empty() bool {
 	return dq.Size() == 0
 }
 
+// Size returns the number of elements in the deque.
 func (dq *Deque[T]) Size() int {
 	return len(dq.FrontStack) + len(dq.BackStack)
 }
 
+// Front returns the front element of deque.
 func (dq *Deque[T]) Front() T {
 	if dq.Empty() {
 		panic("Deque: empty")
@@ -32,6 +37,7 @@ func (dq *Deque[T]) Front() T {
 	return dq.FrontStack[len(dq.FrontStack)-1]
 }
 
+// Back returns the back element of deque.
 func (dq *Deque[T]) Back() T {
 	if dq.Empty() {
 		panic("Deque: empty")
@@ -42,6 +48,7 @@ func (dq *Deque[T]) Back() T {
 	return dq.BackStack[len(dq.BackStack)-1]
 }
 
+// At returns the i th element of deque.
 func (dq *Deque[T]) At(i int) T {
 	if !dq.isValidIdx(i) {
 		panic("Deque: index out of range")
@@ -52,14 +59,17 @@ func (dq *Deque[T]) At(i int) T {
 	return dq.BackStack[i-len(dq.FrontStack)]
 }
 
+// PushFront adds the element x to the front of deque.
 func (dq *Deque[T]) PushFront(x T) {
 	dq.FrontStack = append(dq.FrontStack, x)
 }
 
+// PushBack adds the element x to the back of deque.
 func (dq *Deque[T]) PushBack(x T) {
 	dq.BackStack = append(dq.BackStack, x)
 }
 
+// PopFront removes and returns the front element of deque.
 func (dq *Deque[T]) PopFront() T {
 	if dq.Empty() {
 		panic("Deque: empty")
@@ -74,6 +84,7 @@ func (dq *Deque[T]) PopFront() T {
 	return res
 }
 
+// PopBack removes and returns the back element of deque.
 func (dq *Deque[T]) PopBack() T {
 	if dq.Empty() {
 		panic("Deque: empty")
